f83049e2a4d4c3dd3ba1bd64dc5369d8b4f19f9c
Juan Andrés <juanandres@alboradait.com>
2022-10-14 14:59:45 +0200

    fix

--- src/components/SokobanGame.tsx
+++ src/components/SokobanGame.tsx
@@ -1,51 +1,62 @@
-import { useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 type SokobanBoardProps = {
   board: SokobanBoard
 }
 
 export default function SokobanGame({ board }: SokobanBoardProps) {
-  const size = useRef(Math.max(board.board.length, board.board[0]?.length))
 
-  const renderCell = (cell: SokobanCell) =>
+  const renderCell = (cell: SokobanCell, key: string) =>
     ({
-      BLOCK: <div className="bg-purple-900 aspect-square"></div>,
-      FREE: <div className="bg-purple-300 aspect-square"></div>,
-      BUTTON: <div className="bg-purple-50 aspect-square"></div>,
+      BLOCK: <div key={key} className="bg-purple-900 aspect-square"></div>,
+      FREE: <div key={key} className="bg-purple-300 aspect-square"></div>,
+      BUTTON: <div key={key}className="bg-purple-50 aspect-square"></div>,
     }[cell])
 
   const renderBoard = () => {
-    const halfYWalls = (size.current - board.board.length) / 2
-    const halfXWalls = (size.current - board.board[0].length) / 2
+    if(!size) return null
+    const halfYWalls = (size - board.board.length) / 2
+    const halfXWalls = (size - board.board[0].length) / 2
+    let index = 0;
 
     return [
-      Array((1 + Math.floor(halfYWalls)) * (size.current + 2))
+      Array((1 + Math.floor(halfYWalls)) * (size + 2))
         .fill('')
-        .map(() => renderCell('BLOCK')),
+        .map(() => renderCell('BLOCK', String(index++))),
       board.board.map((row) => [
         Array(1 + Math.floor(halfXWalls))
           .fill('')
-          .map(() => renderCell('BLOCK')),
-        row.map((item) => renderCell(item)),
+          .map(() => renderCell('BLOCK', String(index++))),
+        row.map((item) => renderCell(item, String(index++))),
         Array(1 + Math.ceil(halfXWalls))
           .fill('')
-          .map(() => renderCell('BLOCK')),
+          .map(() => renderCell('BLOCK', String(index++))),
       ]),
-      Array((1 + Math.ceil(halfYWalls)) * (size.current + 2))
+      Array((1 + Math.ceil(halfYWalls)) * (size + 2))
         .fill('')
-        .map(() => renderCell('BLOCK')),
+        .map(() => renderCell('BLOCK', String(index++))),
     ]
   }
+
+  const [size, setSize] = useState<number | null>(null)
+  const [sokobanBoard, setSokobanBoard] = useState<JSX.Element[] | JSX.Element[][][] | null>(null)
+
+  useEffect(() => {
+    setSize(Math.max(board.board.length, board.board[0]?.length)),
+    setSokobanBoard(renderBoard())
+  }, [board])
+
+  console.log("render")
 
   return (
     <div
       role="grid"
       className="grid w-full aspect-square rounded-xl border-4 border-violet-600 max-w-full max-h-full"
       style={{
-        gridTemplateColumns: `repeat(${size.current + 2}, 1fr)`,
+        gridTemplateColumns: `repeat(${(size || 0) + 2}, 1fr)`,
       }}
     >
-      {renderBoard()}
+      {sokobanBoard}
     </div>
   )
 }
