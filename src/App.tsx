41f6df68fdabb59cd28242675fbed7f0e5901215
Juan Andrés <juanandres@alboradait.com>
2022-10-14 09:09:13 +0200

    code spliting

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,65 @@
+import { ReactNode, Suspense, lazy } from 'react'
 import { MdHelp, MdOutlinePoll, MdSettings } from 'react-icons/md'
-import { Link, Route, Switch } from 'wouter'
+import { Link, Route, Router, Switch } from 'wouter'
 
-import Config from './pages/Config'
-import Help from './pages/Help'
-import Index from './pages/Index'
-import Stats from './pages/Stats'
+const loadingFallback = (): ReactNode => <p>Loading...</p>
+
+const Index = lazy(() => import('./pages/Index'))
+const Stats = lazy(() => import('./pages/Stats'))
+const Config = lazy(() => import('./pages/Config'))
+const Help = lazy(() => import('./pages/Help'))
 
 function App() {
   return (
-    <div className="flex h-screen flex-col">
-      <header className="flex flex-none flex-row justify-center bg-slate-900 py-2">
-        <nav className="flex w-full max-w-[500px] flex-row justify-center">
-          <Link href="/help" className="flex-none">
-            <a className="grid place-items-center rounded-md p-2 text-2xl text-white transition hover:bg-gray-500 hover:bg-opacity-50">
-              <MdHelp></MdHelp>
-            </a>
-          </Link>
-          <div className="w-[40px] flex-none"></div>
-          <div className="flex-auto"></div>
-          <Link href="/" className="flex-none">
-            <a className="grid place-items-center rounded-md p-2 transition hover:bg-gray-500 hover:bg-opacity-50">
-              <h1 className="bg-gradient-to-br from-indigo-700 to-red-600 bg-clip-text text-center text-4xl font-bold text-transparent ">
-                Sokoble
-              </h1>
-            </a>
-          </Link>
-          <div className="flex-auto"></div>
-          <Link href="/stats" className="flex-none">
-            <a className="grid place-items-center rounded-md p-2 text-2xl text-white transition hover:bg-gray-500 hover:bg-opacity-50">
-              <MdOutlinePoll></MdOutlinePoll>
-            </a>
-          </Link>
-          <Link href="/config" className="flex-none">
-            <a className="grid place-items-center rounded-md p-2 text-2xl text-white transition hover:bg-gray-500 hover:bg-opacity-50">
-              <MdSettings></MdSettings>
-            </a>
-          </Link>
-        </nav>
-      </header>
-      <main className="flex-auto bg-slate-800">
-        <div className="h-full w-full max-w-[500px] mx-auto">
-          <Switch location=''>
-            <Route path="/sokoble" component={Index}></Route>
-            <Route path="/sokoble/stats" component={Stats}></Route>
-            <Route path="/sokoble/config" component={Config}></Route>
-            <Route path="/sokoble/help" component={Help}></Route>
-          </Switch>
-        </div>
-      </main>
-      <footer className="flex flex-none flex-row justify-center bg-slate-900 py-1">
-        <span className="text-center text-sm text-white">ⓒAxford 2022</span>
-      </footer>
-    </div>
+    <Router base="/sokoble">
+      <div className="flex h-screen flex-col">
+        <header className="flex flex-none flex-row justify-center bg-slate-900 py-2">
+          <nav className="flex w-full max-w-[500px] flex-row justify-center">
+            <Link href="/help" className="flex-none">
+              <a className="grid place-items-center rounded-md p-2 text-2xl text-white transition hover:bg-gray-500 hover:bg-opacity-50">
+                <MdHelp></MdHelp>
+              </a>
+            </Link>
+            <div className="w-[40px] flex-none"></div>
+            <div className="flex-auto"></div>
+            <Link href="/" className="flex-none">
+              <a className="grid place-items-center rounded-md p-2 transition hover:bg-gray-500 hover:bg-opacity-50">
+                <h1 className="bg-gradient-to-br from-indigo-700 to-red-600 bg-clip-text text-center text-4xl font-bold text-transparent ">
+                  Sokoble
+                </h1>
+              </a>
+            </Link>
+            <div className="flex-auto"></div>
+            <Link href="/stats" className="flex-none">
+              <a className="grid place-items-center rounded-md p-2 text-2xl text-white transition hover:bg-gray-500 hover:bg-opacity-50">
+                <MdOutlinePoll></MdOutlinePoll>
+              </a>
+            </Link>
+            <Link href="/config" className="flex-none">
+              <a className="grid place-items-center rounded-md p-2 text-2xl text-white transition hover:bg-gray-500 hover:bg-opacity-50">
+                <MdSettings></MdSettings>
+              </a>
+            </Link>
+          </nav>
+        </header>
+        <main className="flex-auto bg-slate-800">
+          <div className="h-full w-full max-w-[500px] mx-auto">
+            <Suspense fallback={loadingFallback()}>
+              <Switch>
+                <Route path="/">{() => <Index />}</Route>
+                <Route path="/stats">{() => <Stats />}</Route>
+                <Route path="/config">{() => <Config />}</Route>
+                <Route path="/help">{() => <Help />}</Route>
+                <Route>404 Page not found</Route>
+              </Switch>
+            </Suspense>
+          </div>
+        </main>
+        <footer className="flex flex-none flex-row justify-center bg-slate-900 py-1">
+          <span className="text-center text-sm text-white">ⓒAxford 2022</span>
+        </footer>
+      </div>
+    </Router>
   )
 }
 
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,15 +2,16 @@ import SokobanGame from "../components/SokobanGame";
 
 export default function Index() {
   return (
-    <SokobanGame
-      board={{
-        board: [
-          ["FREE", "FREE"],
-          ["BLOCK", "BUTTON"],
-        ],
-        player: { x: 0, y: 0 },
-        boxes: [{ x: 1, y: 0 }],
-      }}
-    />
+    <div>Hola</div>
+    // <SokobanGame
+    //   board={{
+    //     board: [
+    //       ["FREE", "FREE"],
+    //       ["BLOCK", "BUTTON"],
+    //     ],
+    //     player: { x: 0, y: 0 },
+    //     boxes: [{ x: 1, y: 0 }],
+    //   }}
+    // />
   )
 }
