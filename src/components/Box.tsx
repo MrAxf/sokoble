ecd6d9eb4a96129f828e3b17574d0031c5a67d8f
Juan Andrés <juanandres@alboradait.com>
2022-10-21 12:19:56 +0200

    boxes desing

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -12,12 +12,14 @@ export default function Box({ x, y }: BoxProps) {
 
   return (
     <motion.div
-      className="absolute aspect-square bg-amber-700"
+      className="absolute aspect-square bg-amber-700 rounded-md border-orange-900 border-4"
       style={{ width: `${meta.squarePercent}%` }}
       animate={{
         left: `${(meta.walls.left + x) * meta.squarePercent}%`,
         top: `${(meta.walls.top + y) * meta.squarePercent}%`,
       }}
-    ></motion.div>
+    >
+      <div className="w-1/2 bg-orange-500 aspect-square m-[25%]"></div>
+    </motion.div>
   )
 }
