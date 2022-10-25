1823b043b0e7b2acb806f4351c22c06f3a2faded
Juan Andrés <juanandres@alboradait.com>
2022-10-25 18:40:03 +0200

    Updates

--- assets/Stats.134e1c86.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index.10d92681.js";function a(){return t("div",{className:"text-white tex-xl",children:"Stats"})}export{a as default};
---
+++ assets/Stats.714afef7.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index.3cbd9f04.js";function a(){return t("div",{className:"text-white tex-xl",children:"Stats"})}export{a as default};
