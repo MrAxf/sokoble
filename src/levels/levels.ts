f22aec38970f0eb8a01b99d2c93c6436916753f2
Juan Andrés <juanandres@alboradait.com>
2022-10-31 09:34:47 +0100

    bugfixs

--- src/levels/levels.ts
+++ src/levels/levels.ts
@@ -617,8 +617,8 @@ const levels = [
 const getRandomLevel = () => {
   let seed = Math.floor(Math.random() * levels.length)
   seed = 19
-  const level = toLevel(levels[seed], `level${seed}`)
-  const db = getFirestore(firebase)
-  setDoc(doc(db, 'levels'), level)
+  return toLevel(levels[seed], `level${seed}`)
+  // const db = getFirestore(firebase)
+  // setDoc(doc(db, 'levels'), level)
 }
 export default getRandomLevel
