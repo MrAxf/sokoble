ed295b62979fae8cb9cbce3a18197d930ce72f98
juan <theaxford@gmail.com>
2022-12-08 16:48:52 +0100

    bugfix test level

--- src/pages/api/level.ts
+++ src/pages/api/level.ts
@@ -22,8 +22,7 @@ export default async function handler(
     (today.getTime() - baseDate.getTime()) / (1000 * 3600 * 24)
   )
 
-  // const levelNo = (day * 53) % count
-  const levelNo = 50
+  const levelNo = (day * 53) % count
 
   const { data } = await supabase
     .from('levels')
